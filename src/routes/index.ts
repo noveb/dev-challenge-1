a74e98887d953f84c79a5991ade037dd4e9f7180
Jochen Wikenhauser <noveb@gmx.de>
2023-05-24 18:50:17 +0200

    :art: Make storage a singleton

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,19 +3,21 @@ import express from 'express';
 import { ListBucketsCommand } from '@aws-sdk/client-s3';
 
 import database from '../database';
-import { s3 } from '../storage';
+import storage from '../storage';
 
 const router = express.Router();
 
 router.get('/', async (req: Request, res: Response) => {
   const db = await database.getDb();
+  const s3 = await storage.getS3();
+
   const result = {
     api_ready: true,
-    db_ready: await db.stats(),
-    storage_ready: await s3.send(new ListBucketsCommand({})),
+    db_ready: !!await db.stats(),
+    storage_ready: !!await s3.send(new ListBucketsCommand({})),
   };
 
-  res.send(result);
+  return res.send(result);
 });
 
 export default router;
--- src/storage/index.ts
+++ src/storage/index.ts
@@ -2,36 +2,50 @@ import {
   Bucket, CreateBucketCommand, ListBucketsCommand, S3Client,
 } from '@aws-sdk/client-s3';
 
-const accessKeyId = process.env.S3_ACCESS_KEY;
-const secretAccessKey = process.env.S3_SECRET_ACCESS_KEY;
-const endpoint = process.env.S3_ENDPOINT;
+export default class Storage {
+  private static s3: S3Client;
 
-if (!accessKeyId || !secretAccessKey || !endpoint) {
-  throw new Error('S3 variables missing');
-}
+  public static bucketName = 'files';
 
-export const s3 = new S3Client({
-  credentials: {
-    accessKeyId,
-    secretAccessKey,
-  },
-  endpoint,
-  region: 'eu-central-1',
-  forcePathStyle: true,
-});
-
-export const bucketName = 'files';
-
-async function initBucket() {
-  const buckets = await s3.send(new ListBucketsCommand({}));
-  const anyBuckets = buckets.Buckets?.length;
-  const ourBucket = buckets.Buckets?.find((bucket: Bucket) => bucket.Name === bucketName);
-  if (!anyBuckets || !ourBucket) {
-    const response = await s3.send(new CreateBucketCommand({
-      Bucket: bucketName,
-    }));
-    console.log(response);
+  public static getS3(): S3Client {
+    if (!this.s3) {
+      this.connect();
+    }
+    return this.s3;
   }
-  return true;
+
+  private static async connect(): Promise<void> {
+    const accessKeyId = process.env.S3_ACCESS_KEY;
+    const secretAccessKey = process.env.S3_SECRET_ACCESS_KEY;
+    const endpoint = process.env.S3_ENDPOINT;
+
+    if (!accessKeyId || !secretAccessKey || !endpoint) {
+      throw new Error('S3 variables missing');
+    }
+
+    this.s3 = new S3Client({
+      credentials: {
+        accessKeyId,
+        secretAccessKey,
+      },
+      endpoint,
+      region: 'eu-central-1',
+      forcePathStyle: true,
+    });
+
+    await this.initBucket();
+    console.log('Storage ready');
+  }
+
+  private static async initBucket(): Promise<void> {
+    const buckets = await this.s3.send(new ListBucketsCommand({}));
+    const anyBuckets = buckets.Buckets?.length;
+    const ourBucket = buckets.Buckets?.find((bucket: Bucket) => bucket.Name === this.bucketName);
+    if (!anyBuckets || !ourBucket) {
+      const response = await this.s3.send(new CreateBucketCommand({
+        Bucket: this.bucketName,
+      }));
+      console.log(response);
+    }
+  }
 }
-initBucket().then(() => console.log('Bucket exists'));
