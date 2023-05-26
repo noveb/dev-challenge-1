b133bec331ec895a2a544914c0778d4b3f097ed1
Jochen Wikenhauser <noveb@gmx.de>
2023-05-26 12:07:25 +0200

    :lock: Add helmet & fix npm vulnerabilities

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import morgan from 'morgan';
+import helmet from 'helmet';
 
 import indexRouter from './routes/index';
 import fileRouter from './routes/file';
 
 const app = express();
+
+app.use(helmet());
 
 app.use(morgan('dev'));
 app.use(express.json());
